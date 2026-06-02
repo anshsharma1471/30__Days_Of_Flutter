d0d9f712ae1ab840657a5a98701fcb0b3fe9baa7
Shubh Sharma <ansh6983@gmail.com>
2026-06-02 22:47:56 +0530

    First commit

---
+++ lib/catalog_home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CatalogHomePage extends StatefulWidget {
+  const CatalogHomePage({super.key});
+
+  @override
+  State<CatalogHomePage> createState() => _CatalogHomePageState();
+}
+
+class _CatalogHomePageState extends State<CatalogHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'this is my second app',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            color: Colors.grey,
+          ),
+          ),
+      ),
+    );
+  }
+}
