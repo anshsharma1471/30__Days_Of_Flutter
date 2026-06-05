58b569217c7fe03a06d8bb981c50d60867af2003
Shubh Sharma <ansh6983@gmail.com>
2026-06-05 07:39:50 +0530

    day-8 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -10,65 +10,97 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   String name = "";
+  bool changeButton = false;
+
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    setState(() {
+      changeButton = true;
+    });
+    await Future.delayed(Duration(seconds: 1));
+     // ignore: use_build_context_synchronously
+      Navigator.pushNamed(context, Routes.homeRoute);
+      setState(() {
+        changeButton = false;
+      });
+                  
+  }
   @override
   Widget build(BuildContext context) {
     return Material(
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.asset("assets/images/login_image.png",
-            fit: BoxFit.cover,
-            ),
-            
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Welcome to my app $name',
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Image.asset("assets/images/login_image.png",
+              fit: BoxFit.cover,
+              ),
+              
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Welcome to my app ${name.isNotEmpty ? name : "Guest"}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  )
+                  ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20,bottom: 20),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Enter Username',
+                        labelText: 'Username',
+                      ),
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {
+                          
+                        });
+                      },
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Username cannot be empty";
+                        } 
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      obscureText: true, //to hide passward, it shows passward in dote form
+                      decoration: InputDecoration(
+                        hintText: 'Enter Passward',
+                        labelText: 'Passward',
+                      ),
+                      validator: (value) {
+                        if(value == null || value.isEmpty) {
+                          return "Passward cannot be empty";
+                        } else if (value.length < 6) {
+                          return "Password length should be at least 6";
+                        }
+                        return null;
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              ElevatedButton (
+                child: Text('Login',
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
-                )
                 ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20,bottom: 20),
-              child: Column(
-                children: [
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Enter Username',
-                      labelText: 'Username',
-                    ),
-                    onChanged: (value) {
-                      name = value;
-                      setState(() {
-                        
-                      });
-                    },
-                  ),
-                  TextField(
-                    obscureText: true, //to hide passward, it shows passward in dote form
-                    decoration: InputDecoration(
-                      hintText: 'Enter Passward',
-                      labelText: 'Passward',
-                    ),
-                  ),
-                ],
+                ),
+                onPressed: () {
+                  if(_formKey.currentState!.validate()) {
+                    moveToHome(context);
+                  }
+                }
               ),
-            ),
-            ElevatedButton (
-              child: Text('Login',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-              ),
-              onPressed: () async {
-                await Future.delayed(Duration(seconds: 1));
-                // ignore: use_build_context_synchronously
-                Navigator.pushNamed(context, Routes.homeRoute);
-                
-              },
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
